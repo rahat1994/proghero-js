5f029cf8763b743abef7b8ce3325bca16a0fafe4
Rahat Baksh <rahat392@gmail.com>
2020-07-16 02:54:39 -0700

    Initial Commit

---
+++ proghero_js_editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proghero_js_editor
+//
+//  Created by Rahat Baksh on 16/7/20.
+//  Copyright © 2020 Rahat Baksh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ proghero_js_editorTests/proghero_js_editorTests.swift
@@ -0,0 +1,34 @@
+//
+//  proghero_js_editorTests.swift
+//  proghero_js_editorTests
+//
+//  Created by Rahat Baksh on 16/7/20.
+//  Copyright © 2020 Rahat Baksh. All rights reserved.
+//
+
+import XCTest
+@testable import proghero_js_editor
+
+class proghero_js_editorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
